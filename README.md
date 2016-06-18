af96a57858e0c5b461420206a9f0e93e49ad8958
ierceg <ivan@softwaremarbles.com>
2016-06-18 12:30:52 -0400

    Add enforce function

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # node-enforce-envvars
+
 Enforces existence of necessary envvars
+
+## Install
+
+```bash
+npm install enforce-envvars --save
+```
+
+## Example
+
+```js
+require('enforce-envvars')(['VAR_X', 'VAR_Y', 'VAR_Z']);
+```
+
+If any of these 3 variables is not defined a new exception will be thrown.
---
+++ index.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+const _ = require('lodash');
+const util = require('util');
+
+module.exports = (envvarOrArrayOfEnvvars) => {
+  const envvars = envvarOrArrayOfEnvvars;
+  if (!_.isArray(envvars)) {
+    envvars = [envvarOrArrayOfEnvvars];
+  }
+
+  _.each(envvars, (envvar) => {
+    if (_.isEmpty(envvar)) {
+      throw new Error(util.format('Process enviroment %s must be defined', envvar));
+    }
+  });
+};
